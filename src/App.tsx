410e4b67bc93da788873cda53dc4c93da6b49ef1
alexanderem49 <alexanderem49@gmail.com>
2023-11-18 23:50:35 +0300

    add proposal hash field and status label

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { MetaMaskInpageProvider } from "@metamask/providers";
 
-import { useReducer } from 'react';
+import { useReducer, useRef } from 'react';
 import { ethers, formatEther } from 'ethers';
 import { ActionButton } from './utlis';
 
@@ -12,18 +12,22 @@ declare global {
 }
 
 type CounterAction =
-  { type: "setWallet"; value: State["walletAddress"] };
+  { type: "setWallet"; value: State["walletAddress"] }|
+  { type: "setProcessResult"; value: State["processResult"] };;
 
-const initialState: State = { walletAddress: "Not connected" };
+const initialState: State = { walletAddress: "Not connected", processResult: "..." };
 
 interface State {
-  walletAddress: string
+  walletAddress: string,
+  processResult: string
 };
 
 function stateReducer(state: State, action: CounterAction): State {
   switch (action.type) {
     case "setWallet":
       return { ...state, walletAddress: action.value };
+    case "setProcessResult":
+      return { ...state, processResult: action.value };
     default:
       throw new Error("Unknown action");
   }
@@ -48,16 +52,39 @@ async function connectMetamask() {
 
 }
 
+async function processProposal(proposalHash: string, provider: ethers.BrowserProvider) {
+  return `Processing proposal ${proposalHash}, connected to ${(await provider.getNetwork()).name}`;
+}
+
 function App() {
   const [state, dispatch] = useReducer(stateReducer, initialState);
+  const inputRef = useRef(null);
 
   const connectAction = async () => dispatch({ type: "setWallet", value: await connectMetamask() });
+    // @ts-ignore-next-line
+    const processAction = async () => dispatch({ type: "setProcessResult", value: await processProposal(inputRef.current.value as string, provider) });
+
 
   return (
     <div className="App">
       <p>
         <ActionButton title="Connect Wallet" action={connectAction}></ActionButton> <br />
         {state.walletAddress}
+      </p>
+
+      <p>
+        Snapshot proposal hash: <input
+          ref={inputRef}
+          type="text"
+          id="message"
+          name="message"
+          autoComplete="off"
+        />
+        <ActionButton title="Process proposal" action={processAction}></ActionButton>
+      </p>
+
+      <p>
+        Process result: {state.processResult}
       </p>
     </div>
   );
