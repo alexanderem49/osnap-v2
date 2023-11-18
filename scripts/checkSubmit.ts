4d73f62c17720d7cce2eb486768d6e38f0002d20
alexanderem49 <alexanderem49@gmail.com>
2023-11-18 21:04:55 +0300

    full settlement

--- scripts/checkSubmit.ts
+++ scripts/checkSubmit.ts
@@ -1,5 +1,6 @@
 import { ethers } from "hardhat";
-import { ActionRouter, Token } from "../typechain-types";
+import { time } from '@nomicfoundation/hardhat-network-helpers'
+import { ActionRouter, Token, OptimisticOracleV3Interface } from "../typechain-types";
 
 async function main() {
     const factory = await ethers.getContractFactory("ActionRouter");
@@ -7,6 +8,7 @@ async function main() {
     const signer = (await ethers.getSigners())[0];
 
     const token = await ethers.getContractAt("Token", "0x07865c6E87B9F70255377e024ace6630C1Eaa37F") as Token;
+    const oov3 = await ethers.getContractAt("OptimisticOracleV3Interface", "0x9923D42eF695B5dd9911D05Ac944d4cAca3c4EAB") as OptimisticOracleV3Interface;
 
     const tokenAmount = ethers.parseUnits("10", await token.decimals());
 
@@ -16,32 +18,35 @@ async function main() {
         console.log("approved")
     }
 
-    try {
-        const txPromise = await contract.postSnapshotResultsAndScheduleExec(
-            "https://snapshot.org/#/stgdao.eth/proposal/0x6c9437b45e8a88978bca68238048fca8c670ed356fa7d4ae9ab9e7e93788c538",
-            [
-                "yes",
-                "no",
-                "abstain"
-            ],
-            [
-                5394687.138654783,
-                196007.85831167529,
-                150077.41939559707
-            ].map((x) => ethers.parseEther(x.toString())),
-            await token.getAddress(),
-            tokenAmount,
-            20,
-            await token.getAddress(),
-            0,
-            "0x",
-            await token.getAddress()
-        );
-    }
-    catch (err) {
-        console.log((await ethers.provider.getTransactionReceipt(err.transactionHash)));
-        console.log(err);
-    }
+    const tx = await contract.postSnapshotResultsAndScheduleExec(
+        "https://snapshot.org/#/stgdao.eth/proposal/0x6c9437b45e8a88978bca68238048fca8c670ed356fa7d4ae9ab9e7e93788c538",
+        [
+            "yes",
+            "no",
+            "abstain"
+        ],
+        [
+            5394687.138654783,
+            196007.85831167529,
+            150077.41939559707
+        ].map((x) => ethers.parseEther(x.toString())),
+        await token.getAddress(),
+        tokenAmount,
+        20,
+        await token.getAddress(),
+        0,
+        "0x",
+        await token.getAddress()
+    );
+    console.log("posted");
+    
+    const assertionMadeEventTopic = "0xdb1513f0abeb57a364db56aa3eb52015cca5268f00fd67bc73aaf22bccab02b7";
+    const assertionId = (await tx.wait())?.logs.find(x => x.topics[0] == assertionMadeEventTopic)?.topics[1];
+
+    await time.increase(20);
+
+    await oov3.settleAndGetAssertionResult(assertionId!);
+    console.log("settled")
 }
 
 main().catch((err) => {
