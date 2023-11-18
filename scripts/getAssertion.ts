a97e61f08e45000872fb63282785129665bd0b00
alexanderem49 <alexanderem49@gmail.com>
2023-11-18 19:10:08 +0300

    Fix: getAssertion

--- scripts/getAssertion.ts
+++ scripts/getAssertion.ts
@@ -4,7 +4,7 @@ async function main() {
     const oov3 = await ethers.getContractAt("OptimisticOracleV3Interface", "0xfb55F43fB9F48F63f9269DB7Dde3BbBe1ebDC0dE");
 
     console.log(
-        await oov3.assertions(
+        await oov3.getAssertion(
             "0xb2f4fc3454cce516eb3b2f8c9edb189e601518e31ae2f0b821e73c973d15d9a3"
         )
     )
